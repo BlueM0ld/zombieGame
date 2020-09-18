207d63cdf96ff25f47db170e59a1ed1e69f944b8
BlueM0ld <blu3m0ld@gmail.com>
2020-09-18 12:46:36 +0100

    add method documentation

--- display.py
+++ display.py
@@ -5,12 +5,14 @@ from music import ToneMusic
 
 class Display:
 
+    @staticmethod
     def displayControls():
         with open('controls.txt', 'r') as f:
             for line in f:
                 print(line.rstrip())
         print()
 
+    @staticmethod
     def title():
         ToneMusic.run("ending")
         with open('zombie.txt', 'r') as f:
